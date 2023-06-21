537016e5c9041690b3e672de521d56afc229a479
Owen <64725743+Ownwn@users.noreply.github.com>
2023-06-22 10:11:50 +1200

    Working command to open the configuration GUI

--- src/main/java/com/ownwn/features/ToggleCommand.java
+++ src/main/java/com/ownwn/features/ToggleCommand.java
@@ -1,30 +1,18 @@
 package com.ownwn.features;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.text.Text;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
-public class ToggleCommand {
-    public static boolean displayOverlay = false;
-    public static long timeDelay = 0;
 
+public class ToggleCommand {
+    public static long timeDelay = 0;
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(
-                literal("stopwatch").executes(ToggleCommand::toggleDisplay) // placeholder command
-        );
+                literal("stopwatch").executes(context -> {
+                    timeDelay = System.currentTimeMillis();
+                    return 0;
+                    // return 0 = success
+                }));
     }
-
-
-    private static int toggleDisplay(CommandContext<FabricClientCommandSource> context) {
-        displayOverlay = !displayOverlay;
-        context.getSource().sendFeedback(Text.of("§6Toggled the overlay to §b" + displayOverlay + "§6."));
-
-        timeDelay = System.currentTimeMillis();
-
-        return 1; // 1 = success
-    }
-
-
 }
